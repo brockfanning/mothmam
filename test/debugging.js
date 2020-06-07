a014b136709d9797708d344752eab2e59bdce4d7
Brock Fanning <brockfanning@gmail.com>
2020-06-07 16:48:32 -0400

    Remove unneeded file

--- test/debugging.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const { convertToHtml } = require('mammoth')
-const docxFromHtml = require('../lib/index')
-const snippets = require('./snippets.json')
-
-//docxFromHtml(snippets['List with emphasis']).then(() => {
-//    console.log('Finished')
-//})
